4a6f8b7afb25629e573a36ffdff8e48cab1371cc
Mauryanx <115839207+Mauryanx@users.noreply.github.com>
2023-05-03 12:06:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from collections import deque
 from heapq import heappush, heappop 
 
 def shortest_shortest_path(graph, source):
-    """
+       """
     Params: 
       graph.....a graph represented as a dict where each key is a vertex
                 and the value is a set of (vertex, weight) tuples (as in the test case)
@@ -12,21 +12,37 @@ def shortest_shortest_path(graph, source):
       a dict where each key is a vertex and the value is a tuple of
       (shortest path weight, shortest path number of edges). See test case for example.
     """
-    ### TODO
-    pass
+  
+  def helper(visited, frontier):
+    if len(frontier) == 0:
+      return visited
+    else:
+      distance, num_edges, node = heappop(frontier)
+      if node in visited:
+        return helper(visited, frontier)
+      else:
+        visited[node] = (distance, num_edges)
+        for neighbor, weight in graph[node]:
+          heappush(frontier, (distance + weight, num_edges + 1, neighbor))
+        return helper(visited, frontier)
+
+  frontier = []
+  heappush(frontier, (0, 0, source))
+  visited = dict()
+  return helper(visited, frontier)
+                
     
 def test_shortest_shortest_path():
 
     graph = {
                 's': {('a', 1), ('c', 4)},
-                'a': {('b', 2)}, # 'a': {'b'},
+                'a': {('b', 2)},
                 'b': {('c', 1), ('d', 4)}, 
                 'c': {('d', 3)},
                 'd': {},
                 'e': {('d', 0)}
             }
     result = shortest_shortest_path(graph, 's')
-    # result has both the weight and number of edges in the shortest shortest path
     assert result['s'] == (0,0)
     assert result['a'] == (1,1)
     assert result['b'] == (3,2)
@@ -35,13 +51,20 @@ def test_shortest_shortest_path():
     
     
 def bfs_path(graph, source):
-    """
+     """
     Returns:
       a dict where each key is a vertex and the value is the parent of 
       that vertex in the shortest path tree.
     """
-    ###TODO
-    pass
+  final = {}
+  frontier = {source}
+  while len(frontier) > 0:
+    source = frontier.pop()
+    for n in graph[source]: 
+      if n not in final.keys():
+        final[n] = source
+        frontier.add(n)
+  return final
 
 def get_sample_graph():
      return {'s': {'a', 'b'},
@@ -60,15 +83,17 @@ def test_bfs_path():
     assert parents['d'] == 'c'
     
 def get_path(parents, destination):
-    """
+  if destination in parents:
+    return get_path(parents, parents[destination]) + parents[destination]
+  else:
+    return ''
+
+def test_get_path():
+     """
     Returns:
       The shortest path from the source node to this destination node 
       (excluding the destination node itself). See test_get_path for an example.
     """
-    ###TODO
-    pass
-
-def test_get_path():
     graph = get_sample_graph()
     parents = bfs_path(graph, 's')
     assert get_path(parents, 'd') == 'sbc'
